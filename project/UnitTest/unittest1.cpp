6b91d1db2b85548844267e501afbb517bb08a0b2
Tomoyuki Oho <oho_cooh@hotmail.com>
2018-12-30 14:07:06 +0900

    * テストプロジェクトを追加

---
+++ project/UnitTest/unittest1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "winapi_utility.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace winapi_utility;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: テスト コードをここに挿入します
+			CString module_directory;
+			GetModuleDirectory(module_directory);
+		}
+
+	};
+}
